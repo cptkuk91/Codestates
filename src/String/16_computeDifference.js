3e884181398b66a4244243ac64f7e39959e67ba5
Kukjin Lee <75208878+cptkuk91@users.noreply.github.com>
2021-10-11 13:49:05 +0900

    CodeStates String 16 Update

--- src/String/16_computeDifference.js
+++ src/String/16_computeDifference.js
@@ -1,3 +1,7 @@
 function computeDifference(num1, num2) {
     return `${num1}, ${num2}의 차이는 ${Math.abs(num1 - num2)}입니다.`;
+    // abs란?
+    // ex)
+    // a-b 에서 음수일 경우 반대값 즉 양수를 반환하고
+    // 결과값이 양수이거나 0이라면 차이를 반환합니다.
 }
