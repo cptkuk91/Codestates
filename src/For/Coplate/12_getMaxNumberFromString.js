f9a48232335b7cf8e93e97fbef7568b5b0470283
Honey Bee <75208878+cptkuk91@users.noreply.github.com>
2021-10-14 23:36:17 +0900

    Github Broken
    Github Broken

---
+++ src/For/Coplate/12_getMaxNumberFromString.js
@@ -0,0 +1,12 @@
+function getMaxNumberFromString(str) {
+    let maxNum = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        if (parseInt(str[i]) > maxNum) {
+            maxNum = parseInt(str[i]);
+        }
+    }
+
+    return String(maxNum);
+}
+
