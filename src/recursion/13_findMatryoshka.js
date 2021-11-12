33c0f72407fcbf4ffa8b38227dc71e626f686c56
이국진 <cptkuk91@gmail.com>
2021-11-12 10:09:48 +0900

    Recursion

---
+++ src/recursion/13_findMatryoshka.js
@@ -0,0 +1,10 @@
+function findMatryoshka(matryoshka, size) {
+    // TODO: 여기에 코드를 작성합니다.
+    if(matryoshka.size === size){
+      return true;
+    } else if(matryoshka.matryoshka && matryoshka.size > size){
+      return findMatryoshka(matryoshka.matryoshka, size);
+    }
+    return false;
+  }
+  
