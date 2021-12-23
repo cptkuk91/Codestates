dc9a4c3584c1f8c9b943ec08beb2267e94b4c6d4
이국진 <cptkuk91@gmail.com>
2021-12-23 22:30:00 +0900

    DreamCoding Array Study

--- src/DreamCoding_Elly/08_Array.js
+++ src/DreamCoding_Elly/08_Array.js
@@ -1,9 +1,18 @@
 // Array
 
 // 1. Declaration
-const arr1 = new Array();
-const arr2 = [1,2,3];
+// const arr1 = new Array();
+// const arr2 = [1,2,3];
 
-// 2. Index Position
+// // 2. Index Position
 const fruits = ['apple', 'banana'];
-console.log(fruits);
+// console.log(fruits);
+// console.log(fruits.length);
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[fruits.length - 1]);
+
+// 3. Looping over an array
+for(let i = 0; i < fruits.length; i++){
+    console.log(fruits[i]);
+}
