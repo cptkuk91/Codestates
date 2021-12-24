836c85b0655ba4b860441a3b0aae1e471964ede1
이국진 <cptkuk91@gmail.com>
2021-12-25 03:18:27 +0900

    Study Promise

--- src/DreamCoding_Elly/12_Promise.js
+++ src/DreamCoding_Elly/12_Promise.js
@@ -1,5 +1,60 @@
 // Promise
 const promise = new Promise((resolve, reject) => {
-    // doing something Haevy
-    console.log('doing something heavy');
+  // doing something Haevy
+  console.log("doing something heavy");
+  setTimeout(() => {
+    resolve("get data");
+    // reject(new Error("no network"));
+  }, 2000);
 });
+
+// consumers then catch finally
+promise
+  .then((value) => {
+    console.log(value);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+  .finally(() => {
+    console.log("Lets study");
+  });
+
+// Promise Chaining
+const fetchNumber = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(1);
+  }, 1000);
+});
+
+fetchNumber
+  .then((num) => num * 2)
+  .then((num) => num * 3)
+  .then((num) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve(num - 1));
+    }, 1000);
+  })
+  .then((num) => console.log(num));
+
+// Error handling
+const getHen = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => resolve("hen"), 1000);
+  });
+
+const getEgg = (hen) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => resolve(`${hen} => egg`), 1000);
+  });
+
+const cook = (egg) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => resolve(`${egg} => meal`), 1000);
+  });
+
+getHen()
+  .then((hen) => getEgg(hen))
+  .then((egg) => cook(egg))
+  .then((meal) => console.log(meal))
+  .catch(console.log(error));
