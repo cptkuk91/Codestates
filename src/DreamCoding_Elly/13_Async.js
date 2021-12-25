962b450d928fdfecd09e03153b3c3e6a0b378499
이국진 <cptkuk91@gmail.com>
2021-12-26 01:35:03 +0900

    Study Async/Await

--- src/DreamCoding_Elly/13_Async.js
+++ src/DreamCoding_Elly/13_Async.js
@@ -2,10 +2,53 @@
 
 // 1. async
 async function fetchUser() {
-    // do network request in 10secs..
-    return 'elly';
+  // do network request in 10secs..
+  return "elly";
 }
 
 const user = fetchUser();
 user.then(console.log);
 console.log(user);
+
+// await
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function getApple() {
+  await delay(1000);
+  // throw 'error';
+  return "apple";
+}
+
+async function getBanana() {
+  await delay(1000);
+  return "banana";
+}
+
+async function pickFruits() {
+  const applePromise = getApple();
+  const bananaPromise = getBanana();
+  const apple = await applePromise;
+  const banana = await bananaPromise;
+
+  return `${apple} + ${banana}`;
+}
+
+pickFruits().then(console.log);
+
+// 유용한 promise 를 통한 async/await
+
+function pickAllFruits() {
+    return Promise.all([getApple(), getBanana()])
+    .then(fruits => fruits.join('+'));
+}
+
+pickFruits().then(console.log);
+
+// ------- 말 그대로 하나만 선착순 출력
+function pickOnlyOne() {
+    return Promise.race([getApple(), getBanana()])
+}
+
+pickOnlyOne().then(console.log);
