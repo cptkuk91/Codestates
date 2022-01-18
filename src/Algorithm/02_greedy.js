665fef8084687c370e333a4540cd73212c62db30
이국진 <cptkuk91@gmail.com>
2022-01-19 00:36:05 +0900

    Coin Count Algorithm

---
+++ src/Algorithm/02_greedy.js
@@ -0,0 +1,34 @@
+function partTimeJob(k) {
+    // TODO: 여기에 코드를 작성하세요.
+  }
+  
+
+// function partTimeJob(k) {
+//     // TODO: 여기에 코드를 작성하세요.
+  
+//     // 거스름돈 K를 0으로 만들어야 한다.
+//     count = 0;
+  
+//     // 최대값 부터 나누기 시작해, 나누지 못한 값을 최소값(1)으로 나누면서 진행한다.
+//     let coin = [500, 100, 50, 10, 5, 1];
+  
+//     // 동전의 최소값 반환
+//     // for (let i = 0; i < coin.length; i++) {
+//     //   if (k > 0) {
+//     //     let coinCount = Math.floor(k / coin[i]);
+//     //     count += coinCount;
+//     //     k = k % coin[i];
+//     //   }
+//     // }
+//     // return count;
+  
+//     for(let m of coin){
+//       if(k > 0){
+//         let coinCount = Math.floor(k / m); // 
+//         count += coinCount;
+//         k = k % m;
+//       }
+//     }
+//     return count;
+//   }
+  
