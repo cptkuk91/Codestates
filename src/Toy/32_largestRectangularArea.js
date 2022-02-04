f3902d59792a928d2fc3866083577c77d8a31469
이세진 <cptkuk91@gmail.com>
2022-02-04 17:48:06 +0900

    Toy는 머리가 아프다

--- src/Toy/32_largestRectangularArea.js
+++ src/Toy/32_largestRectangularArea.js
@@ -1,9 +1,48 @@
+// 뭔말인가..?
+
 const largestRectangularArea = function (histogram) {
-  // TODO: 여기에 코드를 작성합니다.
-  let result = 0;
+  const createMinIdxTree = (arr, ts, te) => {
+    // 가장 작은 값의 '인덱스'를 구하기 위한 구간 트리
+    if (ts === te) return { idx: ts, val: arr[ts] };
 
-  // 가장 작은 직사각형 1값을 구한다. (길이를 알 수 있다.)
-  
+    const mid = parseInt((ts + te) / 2);
+    const left = createMinIdxTree(arr, ts, mid);
+    const right = createMinIdxTree(arr, mid + 1, te);
 
-  return result;
+    return {
+      val: Math.min(left.val, right.val),
+      idx: left.val < right.val ? left.idx : right.idx,
+      left,
+      right,
+    };
+  };
+  const tree = createMinIdxTree(histogram, 0, histogram.length - 1);
+
+  const getMinIdx = (ts, te, rs, re, tree) => {
+    if (rs <= ts && te <= re) return tree.idx;
+    if (te < rs || re < ts) return rs;
+
+    const mid = parseInt((ts + te) / 2);
+    const left = getMinIdx(ts, mid, rs, re, tree.left);
+    const right = getMinIdx(mid + 1, te, rs, re, tree.right);
+    return histogram[left] < histogram[right] ? left : right;
+  };
+
+  const getRangeArea = (start, end) => {
+    if (start > end) return 0;
+    // 현재 구간에서 가장 작은 막대를 찾는다.
+    // 가장 작은 막대이므로 구간의 길이 * 높이만큼의 직사각형을 만들 수 있다. (첫번째 후보)
+    const minIdx = getMinIdx(0, histogram.length - 1, start, end, tree);
+
+    // 가장 작은 막대를 기준으로 왼쪽, 오른쪽 부분에 존재하는 모든 막대의 높이가 더 크다.
+    // 재귀적으로 왼쪽 부분과 오른쪽 부분,
+    // 즉 해당 구간에서 가장 작은 막대를 제외해서 만들 수 있는 가장 큰 직사각형의 넓이를 구한다.
+    return Math.max(
+      (end - start + 1) * histogram[minIdx], // 첫번째 후보
+      getRangeArea(start, minIdx - 1),
+      getRangeArea(minIdx + 1, end)
+    );
+  };
+
+  return getRangeArea(0, histogram.length - 1);
 };
