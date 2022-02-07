10d1c851e0855089b4de812ce04d05ac583e0b0d
이국진 <cptkuk91@gmail.com>
2022-02-07 10:26:49 +0900

    Modify Toy 33

--- src/Toy/33_LIS.js
+++ src/Toy/33_LIS.js
@@ -3,12 +3,12 @@ const LIS = function (arr) {
   // 배열의 길이 최소값은 1로 설정한다.
   let N = arr.length;
   let result = Array(N).fill(1);
-  for(let i = 1; i < N; i++){
-    for(let j = 0; j < i; j++){
-        // 만약 arr[i] 값이 arr[j] 보다 큰 경우 숫자를 하나씩 더해준다.
-        if(arr[i] > arr[j] && result[i] < result[j] + 1){
-            result[i] = result[j] + 1;
-        }
+  for (let i = 1; i < N; i++) {
+    for (let j = 0; j < i; j++) {
+      // 만약 arr[i] 값이 arr[j] 보다 큰 경우 숫자를 하나씩 더해준다.
+      if (arr[i] > arr[j] && result[i] < result[j] + 1) {
+        result[i] = result[j] + 1;
+      }
     }
   }
   return Math.max(...result);
