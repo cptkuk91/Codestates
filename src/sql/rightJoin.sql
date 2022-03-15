c06d2a105a6b1726de1542ade8daf313f0119254
cptkuk91 <cptkuk91@gmail.com>
2022-03-15 23:03:39 +0900

    Covid-19

--- src/sql/rightJoin.sql
+++ src/sql/rightJoin.sql
@@ -4,3 +4,7 @@ FROM table1
 RIGHT JOIN table2 
 ON table1.column_name = table2.column_name;
 
+SELECT Orders.OrderID, Employees.LastName, Employees.FirstName
+FROM Orders
+RIGHT JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID
+ORDER BY Orders.OrderID;
