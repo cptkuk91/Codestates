0321fd4ff7728866dfd094ae7b205d32ae6c71d4
cptkuk91 <cptkuk91@gmail.com>
2022-03-16 23:08:00 +0900

    Toy 33 diff

---
+++ src/Toy/33_LIS2.js
@@ -0,0 +1,21 @@
+const LIS = function (arr) {
+    //TODO: 여기에 코드를 작성합니다.
+    // 배열의 길이 최소값은 1로 설정한다.
+    let N = arr.length;
+    let result = [];
+
+    for(let i = 0; i <= N; i++){
+        result[i] = 1;
+    }
+
+    for (let i = 1; i < N; i++) {
+      for (let j = 0; j < i; j++) {
+        // 만약 arr[i] 값이 arr[j] 보다 큰 경우 숫자를 하나씩 더해준다.
+        if (arr[i] > arr[j] && result[i] < result[j] + 1) {
+          result[i] = result[j] + 1;
+        }
+      }
+    }
+    return Math.max.apply(null, result);
+  };
+  
