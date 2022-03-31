eb88866caed957158da216fba08afc084d727e9c
cptkuk91 <cptkuk91@gmail.com>
2022-03-31 22:40:11 +0900

    Test 2

--- src/HA3/test2.js
+++ src/HA3/test2.js
@@ -15,11 +15,13 @@ function test2 (n, m) {
       result = arr
     } else {
         arr.forEach((fixed, index, origin) => {
-          const rest = [...origin.slice(0, index), ...origin.slice(index + 1)];
-          const permutations = test2(rest, m - 1);
-          const attached = permutations.map((permutation) => [fixed, …permutation]);
-          result.push(...attached);
+          const rest = [...origin.slice(0, index), ...origin.slice(index + 1)]; //?
+          const permutations = test2(rest, m - 1); //?
+          const attached = permutations.map((permutation) => [fixed, ...permutation]); //?
+          result.push(...attached); //?
       });
     }
     return result;
   }
+
+  test2(3,2); //?
